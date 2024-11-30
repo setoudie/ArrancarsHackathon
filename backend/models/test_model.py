e2892213b0b55a8dc796ab3d555d228b89ef4ad8
setoudie <setoudie@gmail.com>
2024-11-30 15:23:06 +0000

    good list_dico results 00

--- backend/models/test_model.py
+++ backend/models/test_model.py
@@ -1,6 +1,5 @@
-from backend.utils import wrangle, translate_text
+from backend.utils import *
 from transformers import pipeline
-
 
 # Initialize the pipeline for text classification
 pipe = pipeline("text-classification", model="phanerozoic/BERT-Sentiment-Classifier")
@@ -22,31 +21,6 @@ show = df["Description"].head(10)
 
 results = pipe(text)
 
-def categorize_sentiments(results):
-    """
-    Catégorise les sentiments en fonction des labels et scores.
-
-    Args:
-        results (list): Liste des résultats contenant 'label' et 'score'.
-
-    Returns:
-        list: Liste des catégories ('negative', 'neutral', 'positive').
-    """
-    categories = []
-    for result in results:
-        label = result['label']
-        score = result['score']
-
-        if label == "LABEL_0" and 0 <= score <= 0.40:
-            categories.append("negative")
-        elif (label == "LABEL_0" or label == "LABEL_1") and 0.40 < score <= 0.60:
-            categories.append("neutral")
-        elif label == "LABEL_1" and 0.60 < score <= 1:
-            categories.append("positive")
-        else:
-            categories.append("unknown")  # Cas non prévu (optionnel)
-
-    return categories
 
 
 for i, (text, result) in enumerate(zip(text, results), start=1):
@@ -55,4 +29,6 @@ for i, (text, result) in enumerate(zip(text, results), start=1):
     print(f"Label: {result['label']}")
     print(f"Score: {result['score']:.2f}")
     print("-" * 400)
+
+print(classify_opinion(df))
 
